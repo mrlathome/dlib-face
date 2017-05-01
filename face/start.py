a111e30fc616e084c2fc1d396ebe2a45a65dc2ee
ahmdrz <ahmadrezazibaei@hotmail.com>
2017-05-01 12:13:33 +0430

    funny works !

--- face/start.py
+++ face/start.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python
 
+from scipy.spatial import distance as dist
 import cv2, os
 import sys
 import numpy as np
 import dlib
+import time
+from imutils import face_utils
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("./data/shape_predictor_68_face_landmarks.dat")
+
+alpha = 0.35
+
+def draw_face(image,dlib_rect):
+    scale_x = (d.right() - d.left())/4
+    scale_y = (d.bottom() - d.top())/4
+    cv2.line(blank_image,(d.left(),d.top()),(d.left()+scale_x,d.top()),(100,200,100))
+    cv2.line(blank_image,(d.left(),d.top()),(d.left(),d.top()+scale_y),(100,200,100))
+    cv2.line(blank_image,(d.right(),d.top()),(d.right()-scale_x,d.top()),(100,200,100))
+    cv2.line(blank_image,(d.right(),d.top()),(d.right(),d.top()+scale_y),(100,200,100))
+    cv2.line(blank_image,(d.left(),d.bottom()),(d.left()+scale_x,d.bottom()),(100,200,100))
+    cv2.line(blank_image,(d.left(),d.bottom()),(d.left(),d.bottom()-scale_y),(100,200,100))
+    cv2.line(blank_image,(d.right(),d.bottom()),(d.right()-scale_x,d.bottom()),(100,200,100))
+    cv2.line(blank_image,(d.right(),d.bottom()),(d.right(),d.bottom()-scale_y),(100,200,100))
+
+def draw_landmark(shape,name,output):    
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS[name]   
+    cv2.drawContours(output, [cv2.convexHull(shape[rStart:rEnd])], -1, (100, 100, 100), 1)
 
 try:
     cam = cv2.VideoCapture(0)
@@ -18,19 +39,24 @@ try:
         dets = detector(image, 1)
         for i, d in enumerate(dets):
             shape = predictor(image, d)
-            landmarks = np.matrix([[p.x, p.y] for p in shape.parts()])
-            for idx, point in enumerate(landmarks):
-                pos = (point[0, 0], point[0, 1])                
-                cv2.circle(blank_image, pos, 1, color=(0, 255, 255))  
-
-            cv2.rectangle(blank_image,(d.left(),d.top()),(d.right(),d.bottom()),(100,200,100),1)
+            shape = face_utils.shape_to_np(shape)          
             
-        cv2.addWeighted(blank_image, 0.5, image, 1 - 0.5, 0, image)
+            draw_landmark(shape,"mouth",blank_image)
+            draw_landmark(shape,"nose",blank_image)
+            draw_landmark(shape,"right_eye",blank_image)
+            draw_landmark(shape,"left_eye",blank_image)
+
+            draw_face(blank_image,d)
+            
+            for point in shape[48:60]:
+                cv2.circle(blank_image,(point[0],point[1]),1,(200,0,0))            
+            
+        cv2.addWeighted(blank_image, alpha, image, 1 - alpha, 0, image)
 
         cv2.imshow('webcam', image)
         key = cv2.waitKey(1) & 0XFF                
         if key == 27: #esc
-            break          
+            break
         
     cam.release()
     cv2.destroyAllWindows()    
@@ -38,6 +64,7 @@ try:
 except KeyboardInterrupt:
     cv2.destroyAllWindows()
     sys.exit(-1)
+
 except Exception:
-    print "error occurred"
-    sys.exit(-2)
+    print "Error Occurred"
+    raise
